8749aedb9f92490111dc85d65926d18a38d13dda
GanzorigA24 <162071835+GanzorigA24@users.noreply.github.com>
2026-09-02 18:36:21 +0000

    Complete Homework 1 Question 3

--- Hw1Q3/MyMain.java
+++ Hw1Q3/MyMain.java
@@ -10,5 +10,16 @@ public class MyMain {
 
         Scanner input = new Scanner(new File("persons.txt"));
         persons.storePersons(input);
+
+        PrintWriter output = new PrintWriter(System.out);
+
+        persons.displayPersons(output);
+        output.flush();
+
+        System.out.println("Person with ID 1002 is at index: " + persons.findPerson("1002"));
+        System.out.println("Person with ID 1005 is at index: " + persons.findPerson("1005"));
+        System.out.println("Person with ID 9999 is at index: " + persons.findPerson("9999"));
+
+        input.close();
     }
 }
